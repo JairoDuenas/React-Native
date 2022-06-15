19e883e10a601bd7fa53777b7b318b2edea75829
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-15 15:10:22 -0500

    Instalando y configurando React Navigation

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
+import "react-native-gesture-handler";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import LoginForm from "./src/components/LoginForm";
 import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Curso React Native</Text>
-      <Saludar firstname="Pedro" lastname={2333} />
-      <Saludar firstname="Andres" lastname="Camacho" />
-      {/* <Saludar firstname="Andres" lastname="Camacho" />
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Curso React Native</Text>
+        <Saludar firstname="Pedro" lastname="Sanchez" />
+        <Saludar firstname="Andres" lastname="Camacho" />
+        {/* <Saludar firstname="Andres" lastname="Camacho" />
       <Saludar firstname="Carlos" lastname="Sanchez" />
       <Saludar firstname="Pedro" lastname="Gonzalez" /> */}
-      <LoginForm />
-    </View>
+        <LoginForm />
+      </View>
+    </NavigationContainer>
   );
 }
 
