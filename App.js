8d648dc560222c3d4ed99117f49b7ed43fccf46c
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-15 12:24:52 -0500

    Propiedades entre componentes: props

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginForm from "./src/components/LoginForm";
+import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Jairo...</Text>
-      <Text>Hola mundo</Text>
+      <Text>Curso React Native</Text>
+      <Saludar name="Andres" />
+      <Saludar name="Carlos" />
+      <Saludar name="Francisco" />
       <LoginForm />
     </View>
   );
